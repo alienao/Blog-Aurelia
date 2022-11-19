dabb9750dfb2e7d5410fed4cd76799ac5e05a86c
alienao <armin.orujyan@gmail.com>
2022-11-19 22:29:50 +0400

    feat: Value Converters and More Aurelia CLI Goodness

---
+++ src/resources/value-converters/date-format.js
@@ -0,0 +1,11 @@
+import moment from 'moment';
+
+export class DateFormatValueConverter {
+  toView(value) {
+    return moment(value).format('MMMM Do YYYY, h:mm a');
+  }
+
+  fromView(value) {
+    //
+  }
+}
